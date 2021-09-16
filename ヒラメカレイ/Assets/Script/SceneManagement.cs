a772f3e59100c15918ab17759fb6f94f19c78106
sugahararyota <s.ryota072@gmail.com>
2021-09-16 13:09:18 +0900

    タイトルからゲームシーンへ遷移できるように、タイトルにボタン追加

--- ヒラメカレイ/Assets/Script/SceneManagement.cs
+++ ヒラメカレイ/Assets/Script/SceneManagement.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
     // Start is called before the first frame update
 
     private string sceneName = "";
-    private bool clearFlag = false;
+    //private bool clearFlag = false;
 
-    public GameObject fade;
-    private bool oneFadeFlag = false;
+    //public GameObject fade;
+    //private bool oneFadeFlag = false;
 
     public enum SceneNames
     {
@@ -23,12 +24,54 @@ public class SceneManagement : MonoBehaviour
     
     void Start()
     {
-        
+        sceneName = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (!oneFadeFlag && fade != null)
+        //{
+        //    //fade.GetComponent<FadeStart>().FadeInA();
+        //    //oneFadeFlag = true;
+        //}
+
+        if(sceneName == SceneNames.TitleScene.ToString())
+        {
+            //Debug.Log("タイトルだよ");
+        }
+        else if (sceneName == SceneNames.GameScene.ToString())
+        {
+
+        }
+        else if (sceneName == SceneNames.ClearScene.ToString())
+        {
+
+        }
+        else if (sceneName == SceneNames.GameOverScene.ToString())
+        {
+
+        }
     }
+
+    public void OnClickTitle()
+    {
+        SceneManager.LoadScene(SceneNames.TitleScene.ToString());
+    }
+
+    public void OnClickGameScene()
+    {
+        SceneManager.LoadScene(SceneNames.GameScene.ToString());
+    }
+
+    public void OnClickEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;   // UnityEditorの実行を停止する処理
+#else
+        Application.Quit();                                // ゲームを終了する処理
+#endif
+    }
+
+
 }
