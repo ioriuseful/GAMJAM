7d272711b30d2902bdf02ed1402e186a29dc5a2c
sugahararyota <s.ryota072@gmail.com>
2021-09-16 11:11:53 +0900

    イグノア変更と各シーン追加

---
+++ ヒラメカレイ/Assets/Script/SceneManagement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private string sceneName = "";
+    private bool clearFlag = false;
+
+    public GameObject fade;
+    private bool oneFadeFlag = false;
+
+    public enum SceneNames
+    {
+        TitleScene,
+        GameScene,
+        ClearScene,
+        GameOverScene,
+    }
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
