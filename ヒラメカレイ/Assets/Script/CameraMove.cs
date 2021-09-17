37adaf3b4bc43e6e6f7d621d70f427cc1c8e934d
sugahararyota <s.ryota072@gmail.com>
2021-09-17 13:10:12 +0900

    ステージの反転のカメラの動き作った

--- ヒラメカレイ/Assets/Script/CameraMove.cs
+++ ヒラメカレイ/Assets/Script/CameraMove.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public GameObject vCamera;
+    public GameObject vCamera2;
+    private bool change = false;
+    private float delay = 0;
+    private const float delayTime = 1.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,18 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(delay > 0)
+        {
+            delay -= 1 * Time.deltaTime;
+            if (delay < 0) delay = 0;
+        }
+
+        if(Input .GetKeyDown(KeyCode.Return) && delay == 0)
+        {
+            vCamera.SetActive(change);
+            change = !change;
+            delay = delayTime;
+        }
     }
 
 
