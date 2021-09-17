33976bc7a6094deb5ddefdf5a337982c78a9a66b
LAPTOP-CT2SJ4KH\machi <machida.1331@gmail.com>
2021-09-17 13:10:09 +0900

    網の処理、プレイやーの動作

---
+++ ヒラメカレイ/Assets/NetControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetControl : MonoBehaviour
+{
+    Vector3 velocity;
+    bool hitFlag;
+    GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new Vector3(0, -0.05f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        transform.position += velocity;
+        if(hitFlag)
+        {
+            velocity.y = 0.3f;
+            Hit(target);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Floor")
+        {
+            velocity.y = 0;
+        }
+        if (other.tag == "Player")
+        {
+            hitFlag = true;
+            target = (other.gameObject);
+        }
+    }
+    void Hit(GameObject player)
+    {
+        player.transform.position = transform.position - new Vector3(0, 1f, 0);
+    }
+    
+}
